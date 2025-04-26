1a5543b486da90d7801220046ec554de24516c3e
IgnacioTobiasGarciaGodoy <nachogg4@outlook.com>
2025-04-26 15:24:10 -0300

    - Crear carpetas 'components' y 'screens' para organizar el proyecto - Especificar la paleta de colores a utilizar - Modificar el theme para aplicar los nuevos colores

--- app/src/main/java/com/ar/mylapp/ui/theme/Color.kt
+++ app/src/main/java/com/ar/mylapp/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.ar.mylapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val GoldDark = Color(0xFFD1AC4B)
+val GoldLight = Color(0xFFFECC04)
+val TransparentBlue = Color(0x112B0A8F)
+val Beige = Color(0xFFE6E0B5)
+val White = Color(0xFFFFFFFF)
+val Red = Color(0xFFFF0202)
+val TransparentDark = Color(0x1C1A14F5)
+val DarkRed = Color(0xFF4D0202)
+val Black = Color(0xFF000000)
+val LightTransparentGray = Color(0xA9A8A48F)
