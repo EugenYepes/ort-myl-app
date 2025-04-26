ee359a1e80b2c5d1e3ee5a413b895c5b8436fa4a
Sofia Laveihle <sofialaveihle@gmail.com>
2025-04-26 17:33:33 -0300

    Creación de componentes: botones

--- app/src/main/java/com/ar/mylapp/ui/theme/Theme.kt
+++ app/src/main/java/com/ar/mylapp/ui/theme/Theme.kt
@@ -2,6 +2,7 @@ package com.ar.mylapp.ui.theme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.ar.mylapp.R
 
 @Composable
 fun MYLAPPTheme(
@@ -9,6 +10,7 @@ fun MYLAPPTheme(
 ) {
     MaterialTheme(
         typography = Typography,
-        content = content
+        content = content,
+        
     )
 }
