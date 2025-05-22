6afed65b83d947368da373e51d71cdae23d7039c
IgnacioTobiasGarciaGodoy <nachogg4@outlook.com>
2025-05-22 00:38:10 -0300

    Remoción de funcionalidad de menú desplegable en Guidebook.kt
    Se eliminó el código relacionado con la visualización y manejo de menús desplegables (`MyDropdownMenu`) y sus datos asociados (`dropdownData`) de la pantalla `Guidebook`.
    
    La pantalla `Guidebook` ahora solo define su estructura básica sin contenido visual específico relacionado con menús desplegables.

--- app/src/main/java/com/ar/mylapp/screens/guidebook/Guidebook.kt
+++ app/src/main/java/com/ar/mylapp/screens/guidebook/Guidebook.kt
@@ -1,16 +1,8 @@
 package com.ar.mylapp.screens.guidebook
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.ar.mylapp.components.entryData.MyDropdownMenu
-import com.ar.mylapp.components.entryData.getDropdownData
 import com.ar.mylapp.viewmodel.DropdownViewModel
 
 @Composable
@@ -18,22 +10,5 @@ fun GuidebookScreen(
     navController: NavController,
     viewModel: DropdownViewModel = viewModel()
 ){
-    val dropdownData = getDropdownData()
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        dropdownData.forEach { (key, options) ->
-            val selectedOptions = viewModel.getSelectedOptions(key)
-
-            MyDropdownMenu(
-                label = key.replaceFirstChar { it.uppercaseChar() },
-                options = options,
-                selectedOptions = selectedOptions,
-                onOptionToggled = { option, isChecked ->
-                    viewModel.onOptionToggled(key, option, isChecked)
-                }
-            )
-
-            Spacer(modifier = Modifier.size(20.dp))
-        }
-    }
 }
