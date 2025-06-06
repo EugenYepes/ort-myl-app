613bcfb3882016c3ab24439ecd85e0da7a63d02c
IgnacioTobiasGarciaGodoy <nachogg4@outlook.com>
2025-06-06 20:49:51 -0300

    Eliminar importaciones no utilizadas en CardCarousel.kt

--- app/src/main/java/com/ar/mylapp/components/card/CardCarousel.kt
+++ app/src/main/java/com/ar/mylapp/components/card/CardCarousel.kt
@@ -1,6 +1,5 @@
 package com.ar.mylapp.components.card
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -18,7 +17,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
